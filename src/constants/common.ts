14709213c6d896b2865d6a8c3da04f05dd861e24
jpalma2002 <justinepalms@gmail.com>
2024-09-23 08:30:30 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Justine Bajenting';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Innovative software developer with a passion for creating intuitive and efficient digital solutions. Strong expertise in coding, debugging, and optimizing applications to ensure high performance and user satisfaction.';
+export const ABOUT_TEXT_2 =
+  'Proficient in full-stack development, specializing in both client-side and server-side programming. Adept at working in agile environments and contributing to the successful delivery of diverse software projects.';
+export const ABOUT_TEXT_3 =
+  'Skilled in leveraging the latest technologies and development frameworks to build scalable, secure, and maintainable applications. Continuously seeking to expand knowledge and improve technical skills.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Experienced Software Engineer with over 10 years in the industry, focusing on web and mobile application development. Deep understanding of JavaScript, React, Node.js, and various backend technologies.';
+export const RESUME_TEXT_2 =
+  'Software Engineer with a decade of experience in designing, developing, and deploying applications using modern JavaScript frameworks and cloud services. Strong problem-solving abilities and a proven track record of delivering high-quality software solutions.';
